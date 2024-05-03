d764276874bcb7c171794a98e70d60520a6c3537
harshitverma2103 <harshitverma2103@gmail.com>
2024-05-03 18:22:20 +0530

    fixwx

--- tests/homePageTest.spec.js
+++ tests/homePageTest.spec.js
@@ -2,11 +2,13 @@ const { test, expect } = require("@playwright/test");
 
 test("Home Page", async ({ page }) => {
   await page.goto("https://www.cvs.com/");
-  
-  await page.click('#sec1-link4');
-  await page.waitForEvent('domcontentloaded');
 
-  await page.click('.cta-deals-banner-text.pulse-text-black.pulse-link');
-  await page.waitForEvent('domcontentloaded');
-  expect(page.url()).toBe("https://www.cvs.com/shop/ecdeals/c/106121?icid=shop-home-spring-renewal-ebr");
+  await page.click("#sec1-link4");
+  await page.waitForEvent("domcontentloaded");
+
+  await page.click(".cta-deals-banner-text.pulse-text-black.pulse-link");
+  await page.waitForEvent("domcontentloaded");
+  expect(page.url()).itoBe(
+    "https://www.cvs.com/extracare/home/?icid=shop-home-extracare-rebrand"
+  );
 });
--- tests/searchForProduct.spec.js
+++ tests/searchForProduct.spec.js
@@ -5,13 +5,11 @@ test("search for the product", async ({ page }) => {
 
   await page.click('#sec6-link12');
   
-  // Wait for the search input field to be visible
   await page.waitForSelector('#cvs-search-input-default-search');
   
   await page.fill('#cvs-search-input-default-search' , 'dove');
   await page.click('#search-submit-default-search');
 
   await page.waitForLoadState('load')
-  // Scroll the page by 500 pixels
   await page.scroll({ top: 500 });
 });
